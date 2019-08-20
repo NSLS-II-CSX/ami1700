427f0c17a1685f523282b84f16d1c6117f8211ea
Andi Barbour <abarbour@bnl.gov>
2019-08-20 14:43:40 -0400

    version need to run ioc at 2ID

--- iocBoot/iocami1700/st.cmd
+++ iocBoot/iocami1700/st.cmd
@@ -12,15 +12,15 @@ dbLoadDatabase("$(TOP)/dbd/ami1700.dbd",0,0)
 ami1700_registerRecordDeviceDriver(pdbbase) 
 
 epicsEnvSet("EPICS_CA_AUTO_ADDR_LIST" , "NO")
-epicsEnvSet("EPICS_CA_ADDR_LIST" ,      "10.23.0.255")
+epicsEnvSet("EPICS_CA_ADDR_LIST" ,      "10.2.0.255")
 epicsEnvSet("STREAM_PROTOCOL_PATH"    , "$(TOP)/protocols")
 
-drvAsynIPPortConfigure("AMI1", "10.23.3.64:7180")
+drvAsynIPPortConfigure("AMI1", "10.2.2.120:7180")
 #asynSetTraceMask("AMI1", 0, 0x9)
 #asynSetTraceIOMask("AMI1", 0, 0x2)
 
 ## Load record instances
-dbLoadRecords("$(TOP)/db/ami1700.db","Sys=XF:23ID1-ES,Dev={HeMeter},PORT=AMI1")
+dbLoadRecords("$(TOP)/db/ami1700.db","Sys=XF:02IDD-ES,Dev={HeMeter},PORT=AMI1")
 
 iocInit()
 
